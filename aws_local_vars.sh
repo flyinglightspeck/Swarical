68f47cc5425043149cc2f6fccc3434dc0f308687
hamed <amzhamed@gmail.com>
2025-06-26 09:02:18 -0700

    test aws

--- aws_local_vars.sh
+++ aws_local_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-N=20 # number of total servers
+N=2 # number of total servers
 USERNAME="ubuntu"
 KEY_PATH="/Users/hamed/Desktop/hamed-vir.pem"
 REPO_PATH="Swarical"
@@ -8,24 +8,6 @@ GITHUB_REPO="https://github.com/flyinglightspeck/Swarical.git"
 now=$(date +%d-%b-%H_%M_%S)
 
 HOSTNAMES=(
-"34.235.89.173"
-"54.197.174.114"
-"54.226.48.67"
-"54.167.91.149"
-"3.89.225.212"
-"34.224.28.167"
-"54.90.115.204"
-"54.174.82.58"
-"34.234.76.72"
-"18.207.92.99"
-"50.17.33.223"
-"52.91.243.57"
-"52.90.59.96"
-"3.87.154.27"
-"52.73.115.143"
-"18.234.217.197"
-"54.205.198.243"
-"54.196.209.202"
-"34.224.80.111"
-"54.167.3.198"
+"3.227.253.101"
+"3.209.12.219"
 )
--- gen_conf.py
+++ gen_conf.py
@@ -44,22 +44,22 @@ props = [
         "keys": ["SHAPE", "GROUP_TYPE", "SS_ERROR_MODEL"],
         "values": [
             # {"SHAPE": "'grid4x4_16_4_spanning_2'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
-            # {"SHAPE": "'chess_100_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
+            {"SHAPE": "'chess_100_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
             # {"SHAPE": "'chess_408_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
             # {"SHAPE": "'palm_725_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
             # {"SHAPE": "'kangaroo_972_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
             # {"SHAPE": "'dragon_1147_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
 
 
-            {"SHAPE": "'skateboard_1372_5_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
-            {"SHAPE": "'skateboard_1372_10_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
-            {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
-            {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "0"},
-            {"SHAPE": "'skateboard_1372_150_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
-            {"SHAPE": "'skateboard_1372_200_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
-
-            {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2'", "SS_ERROR_MODEL": "1"},
-            {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v3'", "SS_ERROR_MODEL": "1"},
+            # {"SHAPE": "'skateboard_1372_5_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
+            # {"SHAPE": "'skateboard_1372_10_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
+            # {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
+            # {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "0"},
+            # {"SHAPE": "'skateboard_1372_150_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
+            # {"SHAPE": "'skateboard_1372_200_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v2'", "SS_ERROR_MODEL": "1"},
+            #
+            # {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2'", "SS_ERROR_MODEL": "1"},
+            # {"SHAPE": "'skateboard_1372_50_spanning_2_sb'", "GROUP_TYPE": "'spanning_2_v3'", "SS_ERROR_MODEL": "1"},
         ]
     },
 ]
