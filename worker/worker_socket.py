26e1c54083b05822e94474c5a63e3a753d69da88
hamed <amzhamed@gmail.com>
2025-06-28 14:14:38 -0700

    increase socket timout

--- worker/worker_socket.py
+++ worker/worker_socket.py
@@ -27,7 +27,7 @@ class WorkerSocket:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         if self.timeout:
-            sock.settimeout(0.05)
+            sock.settimeout(0.1)
         # ttl = struct.pack('b', 1)
         # sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
         sock.bind(Constants.WORKER_ADDRESS)
